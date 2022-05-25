c428c42eb3d5c369b9b15195330f99a82bea7252
NicholasK <nicholask1@outlook.com>
2022-05-25 18:13:48 -0400

    Added tests to test the left arrow button, and fixed bug that had the left arrow move the image forward and not back

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -7,7 +7,7 @@ it("Should render the element", () => {
   render(<Carousel/>);
 })
 
-test("Compare Snaphots", () => {
+test("Compare Snapshots", () => {
   const {asFragment} = render(<Carousel/>);
   expect(asFragment()).toMatchSnapshot();
 })
@@ -28,4 +28,23 @@ it("works when you click on the right arrow", function() {
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
 
+test("works when you click on the left arrow", () => {
+  const {queryByTestId, queryByAltText} = render(<Carousel/>);
 
+  // Gets to the second image
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // Expects to have the second image and not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+  // Moves the image back
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // Expects to have the first image and not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+})
